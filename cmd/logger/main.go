4d56310916ebb604207d13d19b61440a41169cf4
wangfeiping <wangfeiping@outlook.com>
2020-03-09 11:43:53 +0800

    create log repo

---
+++ cmd/logger/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	"github.com/wangfeiping/log"
+)
+
+func main() {
+
+	defer log.Flush()
+
+	log.Trace("init...")
+
+	t := time.Now().Format(time.RFC3339Nano)
+	log.Debugf("debug: %s", t)
+	log.Warn("warn...")
+	log.Error("error...")
+	log.Info("ok, done.")
+}
