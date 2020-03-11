33417d6ba39a6c46749a068e8b3911d4e3f9de4b
wangfeiping <wangfeiping@outlook.com>
2020-03-11 09:06:27 +0800

    Continuous output log

--- cmd/logger/main.go
+++ cmd/logger/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"sync"
 	"time"
 
 	"github.com/wangfeiping/log"
@@ -12,11 +14,45 @@ func main() {
 
 	log.Trace("init...")
 
+	cancel := doLog()
+
+	log.Info("ok, done.")
+
+	keepRunning(cancel)
+}
+
+func output() {
 	t := time.Now().Format(time.RFC3339Nano)
 	log.Debugf("debug: %s", t)
 	log.Warn("warn...")
 	log.Error("error...")
-	log.Info("ok, done.")
+}
 
-	keepRunning(nil)
+func doLog() (cancel context.CancelFunc) {
+	t := time.NewTicker(time.Duration(1) * time.Second)
+	running := true
+	var wg sync.WaitGroup
+	cancel = func() {
+		running = false
+		t.Stop()
+		wg.Wait()
+	}
+	go func() {
+		wg.Add(1)
+		output()
+		for running {
+			select {
+			case <-t.C:
+				{
+					output()
+				}
+			default:
+				{
+					time.Sleep(100 * time.Millisecond)
+				}
+			}
+		}
+		wg.Done()
+	}()
+	return
 }
--- cmd/logger/os.go
+++ cmd/logger/os.go
@@ -8,13 +8,13 @@ import (
 	"github.com/wangfeiping/log"
 )
 
-func keepRunning(callback func(sig os.Signal)) {
+func keepRunning(cancel context.CancelFunc) {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
 
 	select {
 	case s, ok := <-signals:
-		log.Debugf("System signal [%v] %t, trying to run callback...", s, ok)
+		log.Debugf("system signal [%v] %t, trying to run callback...", s, ok)
 		if !ok {
 			break
 		}
